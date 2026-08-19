fab695b0bb8219a518b0f30ef2f491b50bf5c470
hesglyacticms <contacthesgly@gmail.com>
2026-08-19 17:50:56 +0200

    Sync full-stack client entry files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,8 +6,8 @@ import { Route, Switch } from 'wouter';
 import ErrorBoundary from './components/ErrorBoundary';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { About, Applications, Departments, Home, Ownership, Roleplay, Rules, Status } from './pages/SitePages';
-
 function Router() {
+  // make sure to consider if you need authentication for certain routes
   return <Switch>
     <Route path="/" component={Home} />
     <Route path="/about" component={About} />
